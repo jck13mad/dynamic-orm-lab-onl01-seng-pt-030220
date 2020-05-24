839f3007006c242da55fe5974b355a673c475ecb
Jack Madden <jck13mad@gmail.com>
2020-05-24 00:23:27 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,7 +10,7 @@ class InteractiveRecord
   def self.column_names
     DB[:conn].results_as_hash = true 
     
-    table_columns = DB[:conn].execute("PRAGMA table_info(#{")
+    table_columns = DB[:conn].execute("PRAGMA table_info(#{table_name")
   
   
   
